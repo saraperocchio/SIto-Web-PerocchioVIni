09f9768c22d33400f657a82a92e662692c4c03fc
saraperocchio <117842691+saraperocchio@users.noreply.github.com>
2023-04-14 13:49:11 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let slider = document.querySelector('.slider');
+let counter = 1;
+let width = slider.offsetWidth / 3;
+setInterval(() => {
+  if (counter > 2) {
+    counter = 0;
+  }
+  slider.style.transform = `translateX(${-width * counter}px)`;
+  counter++;
+}, 4000);

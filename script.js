f10623df482dc704a86e615f78262f1847472aaa
saraperocchio <117842691+saraperocchio@users.noreply.github.com>
2023-06-09 12:33:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ item.addEventListener("click", function () {
   document.body.classList.toggle("menu-open");
 });
 
+/*torna a inizio pagina*/
+window.addEventListener("scroll", function () {
+  var button = document.querySelector(".back-to-top");
+  if (window.pageYOffset > 300) {
+    button.classList.add("show");
+  } else {
+    button.classList.remove("show");
+  }
+});
+
+document.querySelector(".back-to-top").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
 
